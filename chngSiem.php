ca4cea2e34f94cc0a1bd4c11d378d1ccf82bab98
Jacob <jscheid2713@gmail.com>
2018-04-13 17:25:33 -0400

    Up and running.... Looking good. -CSS tweaks for looks -Form for SIEM IP

--- chngSiem.php
+++ chngSiem.php
@@ -16,19 +16,13 @@
         echo "<h2>Enter the IP address of the SIEM: </h2>";
         echo "</div>";
         ?>
-        <div id='form' class='form' method="post">
-        <form action='/siem.php' class='form'>
+        <div id='form' class='form'>
+        <form action='/chngSiem.php' class='form'>
             IP:<br> 
             <input type="text" name="ipaddr" value="<?php echo $ipaddr;?>"><br><br>
             <input type="submit" value="Submit" size: "1em">
         </form>
         </div>
-        <?php
-            
-
-
-        ?>
-
         
     </body>
     </html>
