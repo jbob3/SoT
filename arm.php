24ba9f08b80a69271a122f192038c779608be25b
Jacob <jscheid2713@gmail.com>
2018-04-15 18:46:41 -0400

    arming rule functionality w/o on off

--- arm.php
+++ arm.php
@@ -1,11 +1,44 @@
+<!DOCTYPE html>
 <html>
-<head></head>
-<body>
+    <head>
+        <meta charset="utf-8" />
+
+        <title>SoT control</title>
+        <link rel="stylesheet" href="index.css" />
+        
+    </head>
+
+    <body>
+        <?php
+        
+        include 'nav.php';
+       
+        echo "<div id='title' class='title'>";
+        echo "<h2>Enter the IP address of the SIEM: </h2>";
+        echo "</div>";
+        ?>
+        <div id="form" class="form">
+        <form action='/arm.php' class="form" method="get">
+            IP:<br> 
+            <input type="submit" value="arm" name="status">ARM</button><br>
+            <input type="submit" value="disarm" name="status">DISMARM</button><br>
+        </form>
+        <hr>
+        </div>
+        <div id='newIP' class='form2'>
+        Now sensor status is now:
+        <input type="text" name="status" value="<?php echo htmlspecialchars($_GET["status"]);?>"><br><br>
+        </div>
+
+    </body>
+    </html>
 
 <?php
+    include 'arming.php';
 
-echo "<h1>Arm or disarm a sensor</h1>"
+    $v[] = array();
+    $v['status'] = $_GET['status'];
 
+    ruleToggle($v['status']);
+    
 ?>
-
-</body>
--- bin/arming.php
+++ bin/arming.php
@@ -11,13 +11,20 @@
     #run sensorStart.sh
 
 #Arm or disarm rules
+function ruleToggle($status){        
     
+    if($status == 'arm'){
+        exec('/opt/scripts/disarmRules.sh');
+    }
+    elseif($status == 'disarm') {
+        exec('/opt/scripts/disarmRules.sh');
+    }
     #apply appropriate toggle
         #If arm cookie run arm.sh
         #if disarm cookie run disarm.sh
 
     #run sensorStart.sh
-
+}
 
 
 
