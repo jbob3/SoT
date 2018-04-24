804a90068f89a7a1250a9f210a09296a3bb3dec5
Jacob <jscheid2713@gmail.com>
2018-04-23 23:03:40 -0400

    fixed on/off

--- bin/sensorStart.sh
+++ bin/sensorStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Startup script for snort
-sudo snort -c /etc/snort/snort.conf -i enp3s0
-~                    
+sudo snort -s -D -c /etc/snort/snort.conf -i enp3s0
+                   
